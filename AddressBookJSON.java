8dbd6610ce68af7466f63e269bb62849ffb4c8fc
anushayadavk <80685122+anushayadavk@users.noreply.github.com>
2021-05-16 13:50:46 -0700

    Add files via upload

---
+++ AddressBookJSON.java
@@ -0,0 +1,44 @@
+package addressbook;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class AddressBookJSON {
+
+    public AddressBookJSON() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+    public void writeContactDetailsInAFile(List<ContactDetail> contactDetailList,String filePath)
+    {
+        try
+        {
+            Gson gson = new Gson();
+            String json = gson.toJson(contactDetailList);
+            FileWriter filewriter = new FileWriter(filePath);
+            filewriter.write(json);
+            filewriter.close();
+        }
+        catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public int readAddressBookFromAJSONFile(String filePath)
+    {
+        AddressBook addressBook = new AddressBook();
+        try {
+            Reader reader = Files.newBufferedReader(Paths.get(filePath));
+            List<ContactDetail> contactDetailList = new Gson().fromJson(reader, new TypeToken<List<ContactDetail>>() {}.getType());
+            contactDetailList.stream().forEach(singleContactEntry -> addressBook.addContactToList(singleContactEntry));
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+        return addressBook.getContactArray().size();
+    }
+}
